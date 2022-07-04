0c64ad8a81ceaec3641b083a3aa562afea3ae39a
DanK1368 <danielkoett@gmail.com>
2022-07-04 15:38:51 +0200

    footer component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,8 @@
 //import
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-
-
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
