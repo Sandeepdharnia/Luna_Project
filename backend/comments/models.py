d278aeafee3b568623a8a92ad8680903f6670003
S.Dharnia <s.dharnia@yahoo.com>
2022-07-04 23:34:22 +0200

    comments added

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+class Comment(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    content = models.CharField(max_length=2000)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    # post = models.ForeignKey(to=User, on_delete=models.SET_NULL, blank=True, null=True)
+    def __str__(self):
+        return f"ID{self.id}: {self.author} commented by{self.author} created on {self.created}"
--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import get_object_or_404
+from rest_framework.generics import GenericAPIView,ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView, get_object_or_404
+from django.views.generic import ListView, DetailView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from comments.models import Comment
+# from comments.permission import IsUser
+from comments.serializers import CommentSerializer, CommentCreateSerializer
+# from comments.permissions import IsNotOwner
+from rest_framework import filters
+
+
+class CreateCommentView(ListCreateAPIView):
+    queryset = Commnet.object.all(
+    permission_classes = []
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['content']
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return CommentCreateSerializer
+        return CommentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class RetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    lookup_url_kwarg = "comment_id"
+    permission = classes = [IsUser]
