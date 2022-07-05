c970e0c2cd464516b58006214c036333fd42b94c
DanielOM <orihuelasdaniel@gmail.com>
2022-07-05 11:59:24 +0200

    adding registration model (works only once), still working on password reset

--- backend/registration/models.py
+++ backend/registration/models.py
@@ -1,6 +1,7 @@
 import random
 
 from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -16,16 +17,30 @@ def code_generator(length=5):
 class Registration(models.Model):
     email = models.CharField(max_length=50)
     username = models.CharField(max_length=30, unique=True)
-    user = models.OneToOneField(to=User, on_delete=models.CASCADE, related_name='profile', blank=True, null=True) #causin problems:team-2-2-app-1  | registration.Registration.user: (fields.E300) Field defines a relation with model 'User', which is either not installed, or is abstract.team-2-2-app-1  | registration.Registration.user: (fields.E307) The field registration.Registration.user was declared with a lazy reference to 'registration.user', but app 'registration' doesn't provide model 'user'.
+    password = models.CharField(max_length=30, unique=True)
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE,
+                                primary_key=True,
+                                related_name='registration_profile',
+                                )
     code = models.CharField(max_length=5, default=code_generator)
 
     def __str__(self):
         return f'ID{self.id}: {self.email}'
 
-#
-# @receiver(post_save, sender=User)
-# def create_registration_profile(sender, instance, *args, **kwargs):
-#     profile, created = Registration.objects.get_or_create(user=instance)
-#     if created:
-#         profile.save()
 
+@receiver(post_save, sender=User)
+def create_registration_profile(sender, instance, *args, **kwargs):
+    profile, created = Registration.objects.get_or_create(user=instance)
+    if created:
+        profile.save()
+
+        user_email = profile.user.email
+        validation_code = profile.code
+
+        send_mail(
+            'Welcome to Team-1 media platform!',
+            f'Felicitations! Last step in order to use your brand new user. Here is your code: {validation_code}',
+            'sit.academy.testing@gmail.com',
+            [f'{user_email}'],
+            fail_silently=False,
+        )
--- backend/registration/views.py
+++ backend/registration/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
 
-# Create your views here.
+from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework.mixins import UpdateModelMixin
+from rest_framework.response import Response
+
+from users.serializers import UserCreateSerializer
+# from users.serializers import UserCreateSerializer, UserValidationSerializer
+
+User = get_user_model()
+
+
+class RegisterUserView(CreateAPIView):
+    permission_classes = []
+
+    queryset = User.objects.all()
+    serializer_class = UserCreateSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(is_active=False)
+
+
+class ValidateUserView(GenericAPIView, UpdateModelMixin):
+    queryset = User.objects.all()
+    serializer_class = UserCreateSerializer
+    permission_classes = []
+
+    def post(self, request, *args, **kwargs):
+        user_instance = self.get_queryset().filter(email=request.data["email"]).get()
+        validation_code = user_instance.registration_profile.code
+
+        if request.data["code"] == validation_code:
+            return self.partial_update(request, *args, **kwargs)
+        else:
+            return Response(data="Wrong validation code", status=400)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_queryset().filter(email=request.data["email"]).get()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        # serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        send_mail(
+            'Welcome to Team-1 media platform!',
+            f'Felicitations! You can now use your brand new user.',
+            'sit.academy.testing@gmail.com',
+            [f'{request.data["email"]}'],
+            fail_silently=False,
+        )
+
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        serializer.save(is_active=True)
