fbb3311d77da616d89de52d94de4d098e8b67e83
DanK1368 <danielkoett@gmail.com>
2022-07-05 11:50:52 +0200

    added initial page set-up and layout, and home page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
 //import
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SharedLayout from "./pages/SharedLayout";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Profile from "./pages/Profile";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Home />} />
+            <Route path="search" element={<Search />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,36 @@
+import "../styles/Home.scss";
+import restaurantImg from "../assets/restaurant_sample.jpg";
+import RestaurantRatingCard from "../components/RestaurantRatingCard";
+import Grid from "../components/Grid";
+import "../styles/ReviewButton.scss";
+
+const Home = () => {
+  return (
+    <main className="home">
+      <div className="home__top-section">
+        <img src={restaurantImg} alt="" />
+        <form className="home__form">
+          <label>
+            <input
+              className="home__input"
+              type="text"
+              name="search"
+              id="name"
+              placeholder="Search..."
+            />
+          </label>
+          <button className="search-btn">Search</button>
+        </form>
+      </div>
+      <div className="home__card-section">
+        <Grid>
+          <RestaurantRatingCard />
+          <RestaurantRatingCard />
+          <RestaurantRatingCard />
+          <RestaurantRatingCard />
+        </Grid>
+      </div>
+    </main>
+  );
+};
+export default Home;
