70cedce9cd105ac4914acf0ce19dc90c6f7be2f2
DanK1368 <danielkoett@gmail.com>
2022-07-05 15:56:30 +0200

    updated restaurant page layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import SharedLayout from "./pages/SharedLayout";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import Profile from "./pages/Profile";
+import SearchRestaurants from "./pages/SearchRestaurants";
+import SearchReviews from "./pages/SearchReviews";
+import SearchUsers from "./pages/SearchUsers";
 
 function App() {
   return (
@@ -12,7 +15,11 @@ function App() {
         <Routes>
           <Route path="/" element={<SharedLayout />}>
             <Route index element={<Home />} />
-            <Route path="search" element={<Search />} />
+            <Route path="search/" element={<Search />}>
+              <Route path="restaurants" element={<SearchRestaurants />} />
+              <Route path="reviews" element={<SearchReviews />} />
+              <Route path="users" element={<SearchUsers />} />
+            </Route>
             <Route path="profile" element={<Profile />} />
           </Route>
         </Routes>
