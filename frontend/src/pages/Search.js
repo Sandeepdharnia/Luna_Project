ccf3b0fa5e0f83b2c5f536c8feb4e704e474b091
DanK1368 <danielkoett@gmail.com>
2022-07-05 15:57:10 +0200

    updated restaurant page layout

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -1,4 +1,39 @@
+import "../styles/Search.scss";
+import { NavLink } from "react-router-dom";
+import Grid from "../components/Grid";
+import RestaurtRatingCard from "../components/RestaurantRatingCard";
+import { Outlet } from "react-router-dom";
+
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <main className="search">
+      <div className="search__form">
+        <form>
+          <label>
+            <input type="text" name="search" placeholder="Search..." />
+          </label>
+          <label>
+            <select name="category" id="category">
+              <option value="italian">Italian</option>
+              <option value="german">German</option>
+              <option value="asian">Asian</option>
+            </select>
+          </label>
+        </form>
+      </div>
+      <div className="search__links">
+        <div className="search__link-container">
+          <NavLink to="restaurants">RESTAURANTS</NavLink>
+        </div>
+        <div>
+          <NavLink to="reviews">REVIEWS</NavLink>
+        </div>
+        <div>
+          <NavLink to="users">USERS</NavLink>
+        </div>
+      </div>
+      <Outlet />
+    </main>
+  );
 };
 export default Search;
