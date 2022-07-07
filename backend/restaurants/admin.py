e54ba7ef572de67ba186f244a4ed09c597747bbf
DanielOM <orihuelasdaniel@gmail.com>
2022-07-07 15:25:56 +0200

    restaurant model with categories, COMMENTED OUT CATEGORIES MODEL

--- backend/restaurants/admin.py
+++ backend/restaurants/admin.py
@@ -5,7 +5,7 @@ from restaurants.models import Restaurant
 
 
 class RestaurantAdmin(admin.ModelAdmin):
-    list_display = ['name', 'street', 'country', 'category']
+    list_display = ['name', 'street', 'country', 'category_id']
 
 
 admin.site.register(Restaurant, RestaurantAdmin)
--- backend/restaurants/serializers.py
+++ backend/restaurants/serializers.py
@@ -1,21 +1,20 @@
 from rest_framework import serializers
 
-from restaurants.models import Restaurant
+from restaurants.models import Restaurant, CATEGORY_CHOICES
 from users.serializers import UserSerializer
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
     # reviews for later
     author = serializers.SerializerMethodField()
-    category = serializers.SerializerMethodField()
 
     def get_author(self, obj):
         return obj.author.username
 
-    def get_category(self, obj):
-        return obj.category.get_type_display()
+    # def get_category(self, instance):
+    #     return instance.category.get_type_display()
 
     class Meta:
         model = Restaurant
-        fields = ['id', 'name', 'category', 'street', 'city', 'zip', 'country', 'website', 'phone_number', 'email', 'price_level',
+        fields = ['id', 'name', 'get_category_display', 'street', 'city', 'zip', 'country', 'website', 'phone_number', 'email', 'price_level',
                   'author']
