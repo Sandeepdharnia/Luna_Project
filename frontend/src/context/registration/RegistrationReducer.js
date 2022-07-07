42147fbf41345a26bc253f9a42f3637a796db46f
DanielOM <orihuelasdaniel@gmail.com>
2022-07-07 18:12:57 +0200

    linking Registration + Validation(works), needs login, FIXED users.model LOCATION = NULL

--- frontend/src/context/registration/RegistrationReducer.js
+++ frontend/src/context/registration/RegistrationReducer.js
@@ -1,10 +1,12 @@
-// const actions = {
-//   actions.addEmail = "addEmail"
-// }
+export const ACTIONS = {
+  VALIDATE_USER: "validate_user",
+  VERIFY_USER: "verify_user",
+}
 
 const registrationReducer = (state, action) => {
   switch (action.type) {
     case "ADD_EMAIL":
+      console.log(action.payload)
       return {
         ...state,
         userValues: { email: action.payload },
@@ -15,6 +17,20 @@ const registrationReducer = (state, action) => {
         ...state,
         statusCode: action.payload,
       };
+
+    case ACTIONS.VALIDATE_USER:
+      // console.log(action.payload.name)
+      return {
+        ...state,
+        userValues: {...state.userValues, [action.payload.target.name]: action.payload.target.value}
+
+      }
+
+    case ACTIONS.VERIFY_USER:
+      return {
+        ...state,
+        userValues: {...state.userValues}
+      }
 
     default:
       return state;
